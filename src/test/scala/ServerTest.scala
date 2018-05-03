b27597dd1bfbb38e0de634f2845957c9806d7921
Max Rockatansky <denisigoshev@gmail.com>
2018-05-03 16:20:48 +0300

    tests refactoring

--- src/test/scala/ServerTest.scala
+++ src/test/scala/ServerTest.scala
@@ -11,12 +11,7 @@ class ServerTest extends FunSuite with Matchers with ScalatestRouteTest {
     * use ~testQuick in sbt terminal for continuous testing
     */
   test("should respond with correct message ") {
-    val gameService = new GameService()
-    val wsClient = WSProbe()
-
-    // WS creates a WebSocket request for testing
-    WS("/?playerName=John", wsClient.flow) ~> gameService.websocketRoute ~>
-      check {
+   assertWebsocket("John") { wsClient =>
         // check response for WS Upgrade headers
         wsClient.expectMessage("welcome John")
         wsClient.sendMessage(TextMessage("hello"))
@@ -24,32 +19,35 @@ class ServerTest extends FunSuite with Matchers with ScalatestRouteTest {
       }
   }
   test("should register player") {
+  assertWebsocket("John"){ wsClient =>
+    wsClient.expectMessage("welcome John")
+  }
+
+  }
+  def assertWebsocket(playerName: String)( assertions: WSProbe => Unit): Unit = {
     val gameService = new GameService()
     val wsClient = WSProbe()
 
     // WS creates a WebSocket request for testing
-    WS("/?playerName=John", wsClient.flow) ~> gameService.websocketRoute ~>
-      check {
-        // check response for WS Upgrade headers
-        wsClient.expectMessage("welcome John")
-      }
+    WS(s"/?playerName=$playerName", wsClient.flow) ~> gameService.websocketRoute ~> check(
+      assertions (wsClient))
   }
 }
 class GameService() extends Directives {
   implicit val actorSystem: ActorSystem = ActorSystem()
   implicit val actorMaterializer: ActorMaterializer = ActorMaterializer()
-  val websocketRoute: Route = (get & parameter("playerName")){ playerName =>
+  val websocketRoute: Route = (get & parameter("playerName")) { playerName =>
     handleWebSocketMessages(flow(playerName))
   }
-  def flow(playerName:String ) : Flow[Message, Message, Any] = Flow.fromGraph(GraphDSL.create() {
+  def flow(playerName: String): Flow[Message, Message, Any] = Flow.fromGraph(GraphDSL.create() {
     implicit builder =>
-    import GraphDSL.Implicits._
-    //    val materialization = builder.materializedValue.map(m => TextMessage("welcome player"))
-    val materialization = builder.materializedValue.map(_ => TextMessage(s"welcome $playerName"))
-    val massagePassingFlow = builder.add(Flow[Message].map(m => m))
-    val merge = builder.add(Merge[Message](2))
-    materialization ~> merge.in(0)
-    merge ~> massagePassingFlow
-    FlowShape(merge.in(1), massagePassingFlow.out)
+      import GraphDSL.Implicits._
+      //    val materialization = builder.materializedValue.map(m => TextMessage("welcome player"))
+      val materialization = builder.materializedValue.map(_ => TextMessage(s"welcome $playerName"))
+      val massagePassingFlow = builder.add(Flow[Message].map(m => m))
+      val merge = builder.add(Merge[Message](2))
+      materialization ~> merge.in(0)
+      merge ~> massagePassingFlow
+      FlowShape(merge.in(1), massagePassingFlow.out)
   })
 }
