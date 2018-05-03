29e5f7b3f14fe64b32f49d7479209c81dd8b7f79
Max Rockatansky <denisigoshev@gmail.com>
2018-05-03 14:49:43 +0300

    Ws channel - the simple test and functionality. Send message through ws and return the same message

--- src/test/scala/ServerTest.scala
+++ src/test/scala/ServerTest.scala
@@ -1,10 +1,9 @@
 import akka.actor.ActorSystem
-import akka.http.scaladsl.model.ws.Message
-import akka.http.scaladsl.model.ws.{BinaryMessage, TextMessage}
-import akka.http.scaladsl.server.Directives
+import akka.http.scaladsl.model.ws.{Message, TextMessage}
+import akka.http.scaladsl.server.{Directives, Route}
 import akka.http.scaladsl.testkit.{ScalatestRouteTest, WSProbe}
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.stream.scaladsl.Flow
 import org.scalatest.{FunSuite, Matchers}
 
 class ServerTest extends FunSuite with Matchers with ScalatestRouteTest {
@@ -14,35 +13,28 @@ class ServerTest extends FunSuite with Matchers with ScalatestRouteTest {
   test("empty") {
     1 shouldBe 1
   }
-
   test("should create empty GameService") {
     val gameService = new GameService()
-
     val wsClient = WSProbe()
 
     // WS creates a WebSocket request for testing
-    WS("/greeter", wsClient.flow) ~> gameService.websocketRoute ~>
+    WS("/", wsClient.flow) ~> gameService.websocketRoute ~>
       check {
         // check response for WS Upgrade headers
-        isWebSocketUpgrade shouldEqual true
+        //  isWebSocketUpgrade shouldEqual true
+        wsClient.sendMessage(TextMessage("hello"))
+        wsClient.expectMessage("hello")
       }
   }
-
 }
-
 class GameService() extends Directives {
-  implicit val actorSystem = ActorSystem()
-  implicit val actorMaterializer = ActorMaterializer()
-  val websocketRoute = get {
+  implicit val actorSystem: ActorSystem = ActorSystem()
+  implicit val actorMaterializer: ActorMaterializer = ActorMaterializer()
+  val websocketRoute: Route = get {
     handleWebSocketMessages(greeter)
   }
   def greeter: Flow[Message, Message, Any] =
-    Flow[Message].mapConcat {
-      case tm: TextMessage =>
-        TextMessage(Source.single("Hello ") ++ tm.textStream ++ Source.single("!")) :: Nil
-      case bm: BinaryMessage =>
-        // ignore binary messages but drain content to avoid the stream being clogged
-        bm.dataStream.runWith(Sink.ignore)
-        Nil
+    Flow[Message].collect {
+      case TextMessage.Strict(txt) => TextMessage(txt)
     }
 }
