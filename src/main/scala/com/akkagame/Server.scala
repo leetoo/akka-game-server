1b2b19e55cf5452f28f572a63e930a44c64dc8b4
Max Rockatansky <denisigoshev@gmail.com>
2018-05-04 14:45:36 +0300

    made test of the ws connection - everything work https://prnt.sc/jdmy3n

--- src/main/scala/com/akkagame/Server.scala
+++ src/main/scala/com/akkagame/Server.scala
@@ -1,4 +1,49 @@
 package com.akkagame
 
-object Server {
+/**
+  * server which bootstrap the service and expose connection to outside world
+  */
+
+
+  import akka.actor.ActorSystem
+  import akka.http.scaladsl.Http
+  import akka.stream.ActorMaterializer
+  import com.akkagame.service.GameService
+
+  import scala.io.StdIn
+
+  object Server {
+
+    def main(args: Array[String]) {
+      implicit val system = ActorSystem()
+      implicit val materializer = ActorMaterializer()
+      // needed for the future map/flatmap in the end
+      implicit val executionContext = system.dispatcher
+
+      /*val requestHandler: HttpRequest => HttpResponse = {
+        case HttpRequest(GET, Uri.Path("/"), _, _, _) =>
+          HttpResponse(entity = HttpEntity(
+            ContentTypes.`text/html(UTF-8)`,
+            "<html><body>Hello world!</body></html>"))
+
+        case HttpRequest(GET, Uri.Path("/ping"), _, _, _) =>
+          HttpResponse(entity = "PONG!")
+
+        case HttpRequest(GET, Uri.Path("/crash"), _, _, _) =>
+          sys.error("BOOM!")
+
+        case r: HttpRequest =>
+          r.discardEntityBytes() // important to drain incoming HTTP Entity stream
+          HttpResponse(404, entity = "Unknown resource!")
+      }*/
+      val gameService = new GameService()
+      val bindingFuture = Http().bindAndHandle(gameService.websocketRoute, "localhost", 8080)
+      println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+      StdIn.readLine() // let it run until user presses return
+      bindingFuture
+        .flatMap(_.unbind()) // trigger unbinding from the port
+        .onComplete(_ => system.terminate()) // and shutdown when done
+
+    }
+
 }
--- src/main/scala/com/akkagame/service/GameService.scala
+++ src/main/scala/com/akkagame/service/GameService.scala
@@ -7,9 +7,9 @@ import akka.stream.scaladsl.{Flow, GraphDSL, Merge, Sink, Source}
 import akka.stream.{ActorMaterializer, FlowShape, OverflowStrategy}
 import com.akkagame.actor._
 
-class GameService() extends Directives {
-  implicit val actorSystem: ActorSystem = ActorSystem()
-  implicit val actorMaterializer: ActorMaterializer = ActorMaterializer()
+class GameService(implicit val actorSystem: ActorSystem, implicit val actorMaterializer: ActorMaterializer) extends Directives {
+  /*implicit val actorSystem: ActorSystem = ActorSystem()
+  implicit val actorMaterializer: ActorMaterializer = ActorMaterializer()*/
   val websocketRoute: Route = (get & parameter("playerName")) { playerName =>
     handleWebSocketMessages(flow(playerName))
   }
